74b6b4491ce077f1d9fc2cf1c22c5eba0d711c24
mistermarco711 <masandoval711@gmail.com>
2024-08-12 09:47:42 -0500

    Creacion logica encriptador

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const entradaUsuario = document.getElementById("txtaEncriptado");
 const salidaUsuario = document.getElementById("txtaDesencriptado");
 
-const divMensajeInicial= document.getElementById("main__mensaje__inicial");
+const divMensajeInicial = document.getElementById("main__mensaje__inicial");
 const divMensajeEncriptado = document.getElementById("main__mensaje__encriptado");
 
 const btnEncriptar = document.getElementById("btnEncriptar");
@@ -9,33 +9,64 @@ const btnDesencriptar = document.getElementById("btnDesencriptar");
 const btnCopiar = document.getElementById("btnCopiar");
 const btnLimpiar = document.getElementById("btnLimpiar");
 
-btnEncriptar.addEventListener("click", encriptarTexto)
-btnDesencriptar.addEventListener("click", desencriptarTexto);
+btnEncriptar.addEventListener("click", mostrarTextoEncriptado)
+btnDesencriptar.addEventListener("click", mostrarTextoDesencriptado);
 btnCopiar.addEventListener("click", copiarTexto);
 btnLimpiar.addEventListener("click", limpiarVentana);
 
 condicionesIniciales();
 
-function condicionesIniciales(){
+function condicionesIniciales() {
     divMensajeEncriptado.style.display = "none";
     divMensajeInicial.style.display = "block";
     entradaUsuario.value = "";
     salidaUsuario.value = "";
 }
 
-function encriptarTexto(){
+function mostrarTextoEncriptado() {
     divMensajeInicial.style.display = "none";
     divMensajeEncriptado.style.display = "block";
+    let mensajeEncriptado = encriptarDesencriptar(entradaUsuario.value, 1);
+    salidaUsuario.value = mensajeEncriptado;
 }
 
-function desencriptarTexto(){
+function mostrarTextoDesencriptado() {
+    let mensajeDesencriptado;
+    if (salidaUsuario.value != "") {
+        mensajeDesencriptado = encriptarDesencriptar(salidaUsuario.value, 2);
+    } else {
+        divMensajeInicial.style.display = "none";
+        divMensajeEncriptado.style.display = "block";
+        mensajeDesencriptado = encriptarDesencriptar(entradaUsuario.value, 2);
+        
+    }
+    salidaUsuario.value = mensajeDesencriptado;
 
 }
 
-function copiarTexto(){
+function encriptarDesencriptar(texto, opcion) {
+    let matrixEncriptado = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    if (opcion == 1) {
+        for (let i = 0; i < matrixEncriptado.length; i++) {
+            if (texto.includes(matrixEncriptado[i][0])) {
+                texto = texto.replaceAll(matrixEncriptado[i][0], matrixEncriptado[i][1]);
+            }
+        }
+        return texto;
+    } else {
+        for (let i = 0; i < matrixEncriptado.length; i++) {
+            if (texto.includes(matrixEncriptado[i][1])) {
+                texto = texto.replaceAll(matrixEncriptado[i][1], matrixEncriptado[i][0]);
+            }
+        }
+        return texto;
+    }
+
+}
+function copiarTexto() {
 
 }
 
-function limpiarVentana(){
+function limpiarVentana() {
     condicionesIniciales();
 }
